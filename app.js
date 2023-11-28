2d908883915f95dc9c62a9751f0c5d4807bc5f58
Xavier-Hsiao <tim88776@gmail.com>
2023-11-28 16:22:00 +0800

    Build: initialize project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { engine } = require("express-handlebars");
+
+app.use(express.static("public"));
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(port, () => {
+  console.log(`Express server is running on http://localhost:${port}`);
+});
