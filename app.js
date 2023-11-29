fadce60e0d809e4026a21bf7b48d3925932bed1a
Xavier-Hsiao <tim88776@gmail.com>
2023-11-29 09:20:20 +0800

    Build: add /shorten basic route logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { engine } = require("express-handlebars");
+const urls = require("./public/jsons/url.json");
 
 app.use(express.static("public"));
 app.engine(".hbs", engine({ extname: ".hbs" }));
@@ -12,6 +13,15 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/shorten", (req, res) => {
+  // 取得 input 值
+  const inputValue = req.query.url;
+  // 檢查 input 值是否空白
+  if (inputValue && inputValue.length > 0) {
+    const shortUrl = "fakeShortUrl";
+    res.render("short", { shortUrl });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Express server is running on http://localhost:${port}`);
